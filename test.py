7a0f78c291f78a9425ad9c3150b2ceafdddb1cfa
1transparentpng <aidens151@nycstudents.net>
2025-09-11 09:08:24 -0400

    added square and BIGSquare

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("hello")
+import turtle
+from turtle import *
+t = Turtle()
+def square(length):
+    for i in range(4):
+        t.left(90)
+        t.forward(length)
+
+# BIGSquare makes a spiral using (amount) of squares, starting from (length) and increasing by the original length given.
+# Example: BIGSquare(60, 5) would make a spiral of 60 squares, starting from a length of 5 and increasing by 5 for every square done.
+def BIGSquare(amount, length):
+    oglength = length
+    print("Making a spiral of squares :D")
+    for i in range(amount):
+        t.speed(0)
+        square(length)
+        t.left(5)
+        length = length + oglength
+BIGSquare(60, 5)
